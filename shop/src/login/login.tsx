b898a988017613758884ec6c2414afc938d04cd6
Basar <ibrahim.demir@sabanciuniv.edu>
2026-03-14 12:29:24 +0300

    created a login form

---
+++ shop/src/login/login.tsx
@@ -0,0 +1,52 @@
+import React, { useState } from "react";
+import "./style.css";
+import SignInForm from "./SignIn.tsx";
+import SignUpForm from "./SignUp.tsx";
+
+export default function Login() {
+  const [type, setType] = useState("signIn");
+  const handleOnClick = (text:any) => {
+    if (text !== type) {
+      setType(text);
+      return;
+    }
+  };
+  const containerClass =
+    "container " + (type === "signUp" ? "right-panel-active" : "");
+  return (
+    <div className="Login">
+      <div className={containerClass} id="container">
+        <SignUpForm />
+        <SignInForm />
+        <div className="overlay-container">
+          <div className="overlay">
+            <div className="overlay-panel overlay-left">
+              <h1>Hi!</h1>
+              <p>
+                To use our online shop please create an account on the website
+              </p>
+              <button
+                className="ghost"
+                id="signIn"
+                onClick={() => handleOnClick("signIn")}
+              >
+                Sign In
+              </button>
+            </div>
+            <div className="overlay-panel overlay-right">
+              <h1>Welcome Back!</h1>
+              <p>Enter your username and password</p>
+              <button
+                className="ghost "
+                id="signUp"
+                onClick={() => handleOnClick("signUp")}
+              >
+                Sign Up
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
