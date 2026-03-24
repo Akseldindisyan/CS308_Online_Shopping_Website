925941b7612ac4c05e4b6c1b740860c42a6c7ff7
ilke demirkir <>
2026-03-24 13:38:28 +0300

    Added home page + very little nav logic

--- shop/src/main.tsx
+++ shop/src/main.tsx
@@ -1,10 +1,18 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-//import './index.css'
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
+import './index.css'
 import App from './App.tsx'
-import Login from "./login/login.tsx"
+import Login from './login/login.tsx'
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Login />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )
