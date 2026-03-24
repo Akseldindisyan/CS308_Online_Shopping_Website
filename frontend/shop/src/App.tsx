dc73f15ed4bb6a2f26e9a409db9664c6122a099a
Umut Yunusoğlu <yunusoglumut@gmail.com>
2026-03-24 18:52:26 +0300

    routing changed

--- frontend/shop/src/App.tsx
+++ frontend/shop/src/App.tsx
@@ -1,11 +1,14 @@
 import { Routes, Route } from "react-router-dom";
 import Login from "./login/login";
 import PMDashboard from "./pm_admin/PMDashboard";
+import AppContent from "./AppContent";
 
 export default function App() {
   return (
     <Routes>
-      <Route path="/" element={<Login />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/" element={<AppContent />} />
+
       <Route path="/pm-admin/*" element={<PMDashboard />} />
     </Routes>
   );
