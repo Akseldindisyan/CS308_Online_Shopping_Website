7a69683a3a5e219fd442f732cfd93fa4a0645366
Ali Yağız <aliyagiz@Ali-MacBook-Air-5.local>
2026-04-02 23:21:04 +0300

    Integrated dotenv

--- backend/src/main/java/com/backend/backend/AccessingDataJpaApplication.java
+++ backend/src/main/java/com/backend/backend/AccessingDataJpaApplication.java
@@ -2,6 +2,9 @@ package com.backend.backend;
 
 import com.backend.backend.persistence.entity.ProductEntity;
 import com.backend.backend.persistence.repository.ProductRepository;
+
+import io.github.cdimascio.dotenv.Dotenv;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
@@ -15,6 +18,10 @@ public class AccessingDataJpaApplication {
     private static final Logger logger = LoggerFactory.getLogger(AccessingDataJpaApplication.class);
 
     public static void main(String[] args) {
+        Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+        System.setProperty("DB_URL", dotenv.get("DB_URL", ""));
+        System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME", ""));
+        System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD", ""));
         SpringApplication.run(AccessingDataJpaApplication.class, args);
     }
 
