df4d27f6d244f51e526556967ad688482ada98cf
Aksel Dindisyan <aksel.dindisyan@sabanciuniv.edu>
2026-04-09 13:41:58 +0300

    Created Tests for ProductService by using Mockito

---
+++ backend/src/test/java/com/backend/backend/persistence/repository/UserRepositoryTest.java
@@ -0,0 +1,13 @@
+package com.backend.backend.persistence.repository;
+
+import com.backend.backend.persistence.entity.ProductEntity;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+
+public class UserRepositoryTest {
+
+
+}
