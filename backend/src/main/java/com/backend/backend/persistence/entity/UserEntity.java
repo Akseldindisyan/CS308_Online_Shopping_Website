42be220903079879887aa8ffabed9816864db1c2
Umut Yunusoğlu <yunusoglumut@gmail.com>
2026-04-17 18:36:25 +0300

    banking service implemented except mail sender

--- backend/src/main/java/com/backend/backend/persistence/entity/UserEntity.java
+++ backend/src/main/java/com/backend/backend/persistence/entity/UserEntity.java
@@ -52,56 +52,6 @@ public class UserEntity {
         this.role = role;
     }
 
-    public UUID getID(){
-        return id;
-    }
-
-    public String getName(){
-        return name;
-    }
-
-    public String getSurname(){
-        return surname;
-    }
-
-    public String getUsername() { return username; }
-
-    public String getEmail(){
-        return email;
-    }
-
-    public String getPassword(){
-        return password;
-    }
-
-    public LocalDate getDateOfBirth(){
-        return dateOfBirth;
-    }
-
-    public String getAddress(){
-        return address;
-    }
-
-    public void setName(String name){
-        this.name = name;
-    }
-
-    public void setSurname(String surname){
-        this.surname = surname;
-    }
-
-    public void setEmail(String email){
-        this.email = email;
-    }
-
-    public void setPassword(String password){
-        this.password = password;
-    }
-
-    public void setDateOfBirth(LocalDate dateOfBirth){ 
-        this.dateOfBirth = dateOfBirth;
-    }
-
     public void addAddress(AddressEntity addressEntity){
         this.address.add(addressEntity);
     }
