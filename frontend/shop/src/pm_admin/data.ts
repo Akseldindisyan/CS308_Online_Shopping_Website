c471104e67104c92d892b87f4af910d9003ad00b
ilke demirkir <>
2026-05-07 08:44:13 +0300

    Removal of Mock Data

--- frontend/shop/src/pm_admin/data.ts
+++ frontend/shop/src/pm_admin/data.ts
@@ -1,5 +1,9 @@
-export type { Category, Product, Comment, Delivery, Invoice } from "../data";
-export { categories as initCategories, products as initProducts } from "../data";
-export { invoices as initInvoices } from "../data";
-export { deliveries as initDeliveries } from "../data";
-export { comments as initComments } from "../data";
+export type { Category, Product, Comment, Delivery, Invoice, WishlistEntry } from "../data";
+export {
+  categories as initCategories,
+  products as initProducts,
+  invoices as initInvoices,
+  deliveries as initDeliveries,
+  comments as initComments,
+  wishlist as initWishlist,
+} from "../data";
